cb2c68407e02f70f76194d168b850f6a85c7d067
Mariana Porto <84824402+mariporto13@users.noreply.github.com>
2023-12-09 02:46:15 -0300

    Update checkout.js

--- backend/checkout.js
+++ backend/checkout.js
@@ -35,31 +35,23 @@ addform.addEventListener("submit",(ele)=>{
     // taking values of all inputs
     // console.log(address);
 
-    addform.innerHTML = `<div id="html">
-    <input type="radio"  name="payment" value="Pay_debit_card" class="msg">
-    <label for="Pay_debit_card">Pay with Debit/Credit/ATM Cards</label>
-</div>
-<div id="css">
-    <input type="radio"  name="payment" class="msg" value="Net_Banking">
-    <label for="Net_Banking">Net Banking</label><br>
-</div>
-<div id="javascript">
+    addform.innerHTML = `<div id="javascript">
     <input type="radio"  name="payment" class="msg" value="cash">
-    <label for="cash">Cash on Delivery</label><br>
+    <label for="cash">Pay on Delivery</label><br>
 </div>
 <button id="continue">Use this payment method<button>`;  
 let continues = document.getElementById("continue");
 // console.log();
 
-continues.innerText = "confirm order";
+continues.innerText = "Confirm order";
 if(continues.innerText == "confirm order"){
 continues.addEventListener("click",()=>{
         // window.location.replace("../frontend/home.html");
         body.innerHTML = `<div id="confirmation">
         <img src="../database/images/Checklist-Logo.png" alt="">
-        <h2>order confirmed!</h2>
-        <p>your order has been placed successfully !!</p>
-        <p>get delivery by tommorow 9am</p>
+        <h2>Order confirmed!</h2>
+        <p>Your order has been placed successfully !!</p>
+        <p>Get delivery by tomorrow 9am</p>
         <a href="../frontend/products.html"><button>Continue shopping</button></a>
     </div>`;
 let add_data = document.getElementsByClassName("msg").value;
